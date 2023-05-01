9a14d3f359b38c84447990388e2d1b38b43dc594
mahfuj890 <mahfujali490@gmail.com>
2023-05-02 00:35:21 +0600

    add image hover effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,94 +9,151 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.set("#bikeCursor", { xPercent: -50, yPercent: -50 });
 gsap.set(".cursor_area", { xPercent: -50, yPercent: -50 });
 
-window.addEventListener("mousemove", (e) => {
-  const { clientX, clientY } = e;
+// window.addEventListener("mousemove", (e) => {
+//   const { clientX, clientY } = e;
 
-  gsap.to("#bikeCursor", {
-    x: clientX,
-    y: clientY,
-    duration: 0.5,
-    delay: 0.2,
-    ease: "sing.in",
-  });
+//   gsap.to("#bikeCursor", {
+//     x: clientX,
+//     y: clientY,
+//     duration: 0.5,
+//     delay: 0.2,
+//     ease: "sing.in",
+//   });
 
-  gsap.to(".cursor_area", {
-    x: clientX,
-    y: clientY,
-    duration: 0.5,
-    ease: "sing.in",
-    delay: 0.3,
+//   gsap.to(".cursor_area", {
+//     x: clientX,
+//     y: clientY,
+//     duration: 0.5,
+//     ease: "sing.in",
+//     delay: 0.3,
+//   });
+// });
+
+// //Show cursor effect for link,button
+// const getLinkEl = document.querySelectorAll("a,button");
+// const getScaleEl = document.querySelectorAll(".scale_lg");
+
+// if (getLinkEl) {
+//   getLinkEl.forEach((item) => {
+//     item.addEventListener("mouseleave", (e) => {
+//       gsap.to(".cursor_area", {
+//         x: e.clientX,
+//         y: e.clientY,
+//         duration: 0.5,
+//         ease: "sing.in",
+//         delay: 0.3,
+//         scale: 1,
+//         mixBlendMode: "normal",
+//       });
+//       gsap.to("#bikeCursor", {
+//         display: "block",
+//       });
+//     });
+//     item.addEventListener("mouseenter", (e) => {
+//       gsap.to(".cursor_area", {
+//         x: e.clientX,
+//         y: e.clientY,
+//         duration: 0.5,
+//         ease: "sing.in",
+//         delay: 0.3,
+//         scale: 1.2,
+//         mixBlendMode: "difference",
+//       });
+//       gsap.to("#bikeCursor", {
+//         display: "none",
+//       });
+//     });
+//   });
+// }
+// if (getScaleEl) {
+//   getScaleEl.forEach((item) => {
+//     item.addEventListener("mouseleave", (e) => {
+//       gsap.to(".cursor_area", {
+//         x: e.clientX,
+//         y: e.clientY,
+//         duration: 0.5,
+//         ease: "sing.in",
+//         delay: 0.3,
+//         scale: 1,
+//         mixBlendMode: "normal",
+//       });
+//       gsap.to("#bikeCursor", {
+//         display: "block",
+//       });
+//     });
+//     item.addEventListener("mouseenter", (e) => {
+//       gsap.to(".cursor_area", {
+//         x: e.clientX,
+//         y: e.clientY,
+//         duration: 0.5,
+//         ease: "sing.in",
+//         delay: 0.3,
+//         scale: 3,
+//         mixBlendMode: "difference",
+//       });
+//       gsap.to("#bikeCursor", {
+//         display: "none",
+//       });
+//     });
+//   });
+// }
+
+// Hover Image Effect
+let hoverImageEl = gsap.utils.toArray(".hover_img_text"),
+  imageWrapperEl = document.querySelector(".hover_img_area"),
+  imageItemEl = document.querySelector(".hover_img_area img");
+console.log("hoverImageEl", hoverImageEl);
+
+const mouseMove = (e) => {
+  let mouseX = e.clientX,
+    mouseY = e.clientY;
+  let tl = gsap.timeline();
+  tl.to(imageWrapperEl, {
+    duration: 1,
+    x: mouseX + 100,
+    y: mouseY - 80,
+    transformOrigin: "50% 50%",
+    ease: "sign.in",
   });
+};
+
+//Hover Image Change
+const hoverImageChange = (e) => {
+  if (e.type == "mouseenter") {
+    const getImageSrc = e.target.dataset.src;
+    let tl = gsap.timeline();
+    tl.set(imageItemEl, { attr: { src: getImageSrc } }).to(imageWrapperEl, {
+      autoAlpha: 1,
+      visibility: "visible",
+      scale: 1,
+      duration: 1,
+    });
+  } else if (e.type == "mouseleave") {
+    let tl = gsap.timeline();
+    tl.to(imageWrapperEl, {
+      autoAlpha: 0,
+      visibility: "hidden",
+      scale: 0.3,
+      duration: 0.5,
+    });
+  }
+};
+
+const initialAnimation = (e) => {
+  hoverImageEl.forEach((item) => {
+    item.addEventListener("mouseenter", hoverImageChange);
+    item.addEventListener("mouseleave", hoverImageChange);
+    item.addEventListener("mousemove", mouseMove);
+  });
+};
+
+const init = () => {
+  initialAnimation();
+};
+
+window.addEventListener("load", () => {
+  init();
 });
-
-//Show cursor effect for link,button
-const getLinkEl = document.querySelectorAll("a,button");
-const getScaleEl = document.querySelectorAll(".scale_lg");
-
-if (getLinkEl) {
-  getLinkEl.forEach((item) => {
-    item.addEventListener("mouseleave", (e) => {
-      gsap.to(".cursor_area", {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.5,
-        ease: "sing.in",
-        delay: 0.3,
-        scale: 1,
-        mixBlendMode: "normal",
-      });
-      gsap.to("#bikeCursor", {
-        display: "block",
-      });
-    });
-    item.addEventListener("mouseenter", (e) => {
-      gsap.to(".cursor_area", {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.5,
-        ease: "sing.in",
-        delay: 0.3,
-        scale: 1.2,
-        mixBlendMode: "difference",
-      });
-      gsap.to("#bikeCursor", {
-        display: "none",
-      });
-    });
-  });
-}
-if (getScaleEl) {
-  getScaleEl.forEach((item) => {
-    item.addEventListener("mouseleave", (e) => {
-      gsap.to(".cursor_area", {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.5,
-        ease: "sing.in",
-        delay: 0.3,
-        scale: 1,
-        mixBlendMode: "normal",
-      });
-      gsap.to("#bikeCursor", {
-        display: "block",
-      });
-    });
-    item.addEventListener("mouseenter", (e) => {
-      gsap.to(".cursor_area", {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.5,
-        ease: "sing.in",
-        delay: 0.3,
-        scale: 3,
-        mixBlendMode: "difference",
-      });
-      gsap.to("#bikeCursor", {
-        display: "none",
-      });
-    });
-  });
-}
 
 //Top Banner
 
